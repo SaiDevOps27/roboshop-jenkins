769eb24df4b981323b0809eab2cf35e32ce2ccbf
saidevops7722 <saiaws7722@gmail.com>
2024-04-03 18:30:12 +0530

    jenkins provider info

--- vars.tf
+++ vars.tf
@@ -5,12 +5,12 @@ variable "folders" {
 variable "jobs" {
   default = [
     { name = "roboshop", folder = "Infra", repo_url = "https://github.com/SaiDevOps27/roboshop-infra.git" },
-    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/roboshop-infra.git" },
-    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/roboshop-infra.git" },
-    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/roboshop-infra.git" },
-    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/roboshop-infra.git" },
-    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/roboshop-infra.git" },
-    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/roboshop-infra.git" }
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/frontend.git" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/user.git" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/cart.git" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/catalogue.git" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/shipping.git" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/SaiDevOps27/payment.git" }
 
   ]
 }
